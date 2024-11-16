0b1a8a02691fb4220bb0ce1eb70022e8cde939da
teru <isse.teruhi220@mail.kyutech.jp>
2024-11-16 13:53:34 +0000

    add:sampledata

---
+++ src/data/mock/sampleBarGraphWeekSleepScore.ts
@@ -0,0 +1,18 @@
+
+const sampleBarGraphWeekSleepTime = {
+    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+
+    datasets: [
+        {
+            label: 'Sleep Score (points)',
+            data: [
+                1.2, 3.4, 2.1, 4.0, 1.5, 3.6, 2.7
+            ],
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1,
+        },
+    ],
+};
+
+export default sampleBarGraphWeekSleepTime;
