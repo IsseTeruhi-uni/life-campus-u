fa556dbc363ad0c488d5b53c367ffacab7b588ef
teru <isse.teruhi220@mail.kyutech.jp>
2024-11-16 18:16:57 +0000

    add:sampledata

--- src/data/mock/IntakeCalories/sampleBarGraphWeekIntakeCarolies.ts
+++ src/data/mock/IntakeCalories/sampleBarGraphWeekIntakeCarolies.ts
@@ -1,9 +1,13 @@
+
 const sampleBarGraphWeekSleepTime = {
     labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+
     datasets: [
         {
-            label: 'Steps',
-            data: [8945, 10234, 12345, 13456, 14567, 15678, 16789],
+            label: 'Intake Calories (kcal)',
+            data: [
+                2000, 1800, 2200, 1900, 2100, 2000, 1800
+            ],
             backgroundColor: 'rgba(54, 162, 235, 0.2)',
             borderColor: 'rgba(54, 162, 235, 1)',
             borderWidth: 1,
--- src/data/mock/IntakeCalories/sampleBarGraphYearIntakeCarolies.ts
+++ src/data/mock/IntakeCalories/sampleBarGraphYearIntakeCarolies.ts
@@ -1,9 +1,12 @@
-const sampleBarGraphYearSleepTime = {
+
+const sampleBarGraphWeekSleepTime = {
     labels: ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'],
     datasets: [
         {
-            label: 'steps',
-            data: [9830, 10234, 12345, 8765, 7890, 9876, 8765, 7654, 6543, 6432, 5321, 5210],
+            label: 'Intake Calories (kcal)',
+            data: [
+                2000, 1800, 2200, 1900, 2100, 2000, 1800, 2000, 1800, 2200, 1900, 2100
+            ],
             backgroundColor: 'rgba(54, 162, 235, 0.2)',
             borderColor: 'rgba(54, 162, 235, 1)',
             borderWidth: 1,
@@ -11,4 +14,4 @@ const sampleBarGraphYearSleepTime = {
     ],
 };
 
-export default sampleBarGraphYearSleepTime;
+export default sampleBarGraphWeekSleepTime;
