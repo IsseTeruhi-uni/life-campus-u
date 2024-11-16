f962aacd99c6b4f9158b202e142b60a13e1ea009
teru <isse.teruhi220@mail.kyutech.jp>
2024-11-16 13:08:18 +0000

    add:sampledata

---
+++ src/data/mock/sampleBarGraphWeekSleepTime.ts
@@ -0,0 +1,14 @@
+const sampleBarGraphWeekSleepTime = {
+    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    datasets: [
+        {
+            label: 'Sleep Time (hours)',
+            data: [7, 6.5, 8, 7.5, 6, 8.5, 7],
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1,
+        },
+    ],
+};
+
+export default sampleBarGraphWeekSleepTime;
---
+++ src/data/mock/sampleBarGraphYear.ts
@@ -0,0 +1,14 @@
+const sampleBarGraphYearSleepTime = {
+    labels: ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'],
+    datasets: [
+        {
+            label: 'Sleep Time (hours)',
+            data: [6, 5.6, 7.2, 6.8, 6.4, 7.8, 7.2, 6.8, 8.5, 5.5, 6.5, 7.2],
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1,
+        },
+    ],
+};
+
+export default sampleBarGraphYearSleepTime;
